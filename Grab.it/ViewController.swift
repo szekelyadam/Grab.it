5b96b90c4eacb45e7dea214d80ae5594257840f3
szekelyadam <info@szekelyadam.hu>
2016-04-18 23:26:32 +0200

    initializing xcode project

---
+++ Grab.it/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grab.it
+//
+//  Created by Ádám Székely on 18/04/16.
+//  Copyright © 2016 Ádám Székely. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
